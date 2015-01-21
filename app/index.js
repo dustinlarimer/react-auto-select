08d13f487e5615120cdebbaad4eb3a6365cd8dfb
Dustin Larimer <dustinlarimer@gmail.com>
2015-01-20 21:16:48 -0800

    Fugly attempt at scroll correction

--- app/index.js
+++ app/index.js
@@ -14,36 +14,109 @@ var Select = React.createClass({displayName: "Select",
       classes: 'react-select',
       limit: 30,
       userInput: '',
-      visible: false
+      selectedItem: 0,
+      visible: false,
+      visibleItems: 0
     };
   },
 
   resetState: function(){
     this.setState({
-      visible: false,
-      limit: 30
+      limit: 30,
+      selectedItem: 0,
+      visible: false
     });
   },
 
   handleChange: function(e) {
+    // console.log(this.refs);
     this.setState({ userInput: e.target.value });
-    this.refs.scrollpane.getDOMNode().scrollTop = 0;
+    if (this.state.visible) {
+      // this.resetState();
+      this.setState({ limit: 30, selectedItem: 0 });
+      this.refs.scrollpane.getDOMNode().scrollTop = 0;
+    }
+    else {
+      this.setState({ visible: true });
+    }
   },
 
   handleFocus: function(e) {
-    this.setState({ visible: true });
+    if (!this.refs.input.getDOMNode().value.length) {
+      this.setState({ visible: true });
+    }
   },
 
   handleBlur: function(e) {
-    var self = this;
-    setTimeout(self.resetState, 250);
+    // var self = this;
+    this.timeout = setTimeout(this.resetState, 250);
   },
 
-  handleItemSelection: function(index){
+  handleItemFocus: function(index) {
+    clearTimeout(this.timeout);
+  },
+
+  handleItemKeyPress: function(e) {
+    console.log('item-key-down', e);
+    this.setState({ selectedItem: 0 });
+    this.refs.input.getDOMNode().focus();
+  },
+
+  handleItemSelection: function(index) {
     if (typeof this.props.items[index]) {
       this.setState({ userInput: String(this.props.items[index]) });
       this.resetState();
     }
+  },
+
+  handleKeyDown: function(e) {
+    var itemHeight, itemOffset;
+    if (e.keyCode === 9 || e.keyCode === 16) return;
+
+    this.setState({ visible: true });
+    if (this.refs.list) {
+      itemHeight = this.refs.list.getDOMNode().children[this.state.selectedItem].scrollHeight;
+      itemOffset = itemHeight * this.state.selectedItem;
+    }
+
+    switch (e.keyCode) {
+      case 38: // up
+        if (this.state.selectedItem-1 >= 0) {
+          this.setState({ selectedItem: this.state.selectedItem-1 });
+        }
+        else {
+          this.setState({ selectedItem: 0 });
+        }
+        if (this.refs.list && itemOffset > itemHeight*3) {
+          this.refs.scrollpane.getDOMNode().scrollTop -= itemHeight+1;
+        }
+        e.preventDefault();
+        break;
+      case 40: // down
+        if (this.state.selectedItem+1 < this.state.visibleItems) {
+          this.setState({ selectedItem: this.state.selectedItem+1 });
+        }
+        else {
+          this.setState({ selectedItem: this.state.visibleItems-1 });
+        }
+        if (this.refs.list && itemOffset > itemHeight*3) {
+          this.refs.scrollpane.getDOMNode().scrollTop += itemHeight+1;
+        }
+        e.preventDefault();
+        break;
+      case 13: // enter
+        if (this.refs.list) {
+          this.handleItemSelection( this.refs.list.getDOMNode().children[this.state.selectedItem].value );
+        }
+        else {
+          this.resetState();
+          // this.handleFocus();
+        }
+        e.preventDefault();
+        break;
+    }
+
+    // console.log(e.keyCode, e.key, this.state.selectedItem);
   },
 
   handleScroll: function(e) {
@@ -84,20 +157,37 @@ var Select = React.createClass({displayName: "Select",
     return this;
   },
 
+  // React methods
+
+  // componentDidMount: function(){
+  //   console.log('here');
+  //   this.getDOMNode().addEventListener('keydown', function(e){
+  //     console.log('window', e);
+  //   })
+  //   // console.log()
+  // },
+
   render: function(){
     var self = this,
         count = 0;
 
+    self.state.visibleItems = 0;
     var items = this.props.items.map(function(item, index) {
       // If input is present, skip items that don't match
       if (self.state.userInput.length > 0 && String(item).search(self.state.userInput) < 0) return;
 
       // Simple result limiting
       count++; if (count > self.state.limit) return;
-
-      // Bind onClick action
-      var boundClick = self.handleItemSelection.bind(self, index);
-      return React.createElement("li", {className: "react-select-item", key: index, onClick: boundClick}, item)
+      self.state.visibleItems = self.state.visibleItems+1;
+      // console.log(self.state.visibleItems);
+      return React.createElement("li", {className: self.state.selectedItem === count-1 ? "react-select-item active" : "react-select-item", 
+          key: index, 
+          value: item, 
+          onClick: self.handleItemSelection.bind(self, index), 
+          onFocus: self.handleItemFocus.bind(self, index), 
+          onKeyPress: self.handleItemKeyPress.bind(self, index)}, 
+            item
+        );
     });
 
     // Hide scrollpane unless active
@@ -111,7 +201,11 @@ var Select = React.createClass({displayName: "Select",
     }
 
     return React.createElement("div", {ref: "wrapper", className: this.state.classes}, 
-      React.createElement("input", {ref: "input", className: "react-select-input", value: this.state.userInput, onChange: this.handleChange, onFocus: this.handleFocus, onBlur: this.handleBlur}), 
+      React.createElement("input", {ref: "input", className: "react-select-input", value: this.state.userInput, 
+        onChange: this.handleChange, 
+        onFocus: this.handleFocus, 
+        onBlur: this.handleBlur, 
+        onKeyDown: this.handleKeyDown}), 
       scrollpane
     )
   }
